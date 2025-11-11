4e74fa4e6b77d550820071a07946c90c27ac8f65
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-11 19:53:05 +0500

    Variables:var issues V1

---
+++ variable.js
@@ -0,0 +1,17 @@
+
+
+//variables declared by var can be redeclared
+//it is a isuue as when using same variable throughout confusion create confusion
+//like expecting Ahmed but getting Muhammad in lin 14
+
+var a = "Ahmed";
+
+console.log(a)
+
+
+var a = "Muhammad"
+
+console.log("Welcome  " + a)
+
+
+

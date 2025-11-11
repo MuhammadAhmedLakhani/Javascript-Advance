669b8cf593cc5c84a020476749a53dcf4578d8f0
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-11 21:27:14 +0500

    Destructuring:V2

--- destructuring.js
+++ destructuring.js
@@ -3,44 +3,66 @@
 //destructuring allows breaking of object to exteact certain properties
 
 
-let students = {
+// let students = {
 
-    s1:{
-        name: "Ahmed",
-        email: "ahmed@gmail.com"
-    },
-    s2:{
-        name: "Ahmed",
-        email: "ahmed@gmail.com"
-    },
-    s3:{
-        name: "Ahmed",
-        email: "ahmed@gmail.com"
-    },
-    s4:{
-        name: "Ahmed",
-        email: "ahmed@gmail.com"
-    },
-    s5:{
-        name: "Ahmed",
-        email: "ahmed@gmail.com"
+//     s1:{
+//         name: "Ahmed",
+//         email: "ahmed@gmail.com"
+//     },
+//     s2:{
+//         name: "Ahmed",
+//         email: "ahmed@gmail.com"
+//     },
+//     s3:{
+//         name: "Ahmed",
+//         email: "ahmed@gmail.com"
+//     },
+//     s4:{
+//         name: "Ahmed",
+//         email: "ahmed@gmail.com"
+//     },
+//     s5:{
+//         name: "Ahmed",
+//         email: "ahmed@gmail.com"
+//     }
+
+// }
+
+// let {s1}  = students
+
+//   inside curly brakets there should be keys of object
+
+// let { name, email } = s1
+
+
+// console.log(name)
+// console.log(email)
+// console.log(s1)
+
+// //no need to write student.name again and again
+
+// console.log(students)
+
+//it does not affect original object at all
+
+
+
+
+let student  = {
+    name:"Ahmed",
+    email:"ahmed@gmail.com",
+    classDetails:{
+        time : "9-11"
     }
 
 }
 
-let {s1}  = students
-
-//   inside curly brakets there should be keys of object
-
-let { name, email } = s1
+//destructuring multipple objects
+//classDetails is another object which also need to destructure
 
 
-console.log(name)
+let {email , name , classDetails} = student
+let {time} = classDetails
 console.log(email)
-console.log(s1)
-
-//no need to write student.name again and again
-
-console.log(students)
-
-//it does not affect original object at all
+console.log(name)
+console.log(time)

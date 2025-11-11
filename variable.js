98d5255fed2d0572cd12ad852919983fdb19ba56
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-11 20:07:43 +0500

    variables:let benefits

--- variable.js
+++ variable.js
@@ -3,6 +3,7 @@
 //variables declared by var can be redeclared
 //it is a isuue as when using same variable throughout confusion create confusion
 //like expecting Ahmed but getting Muhammad in lin 14
+//var is functional scope
 
 // var a = "Ahmed";
 
@@ -18,9 +19,42 @@
 
 // var support housting which means variable declared with var come on top of program 
 
-console.log(a)
+// console.log(a)
 
-var a = "Ahmed"
+// var a = "Ahmed"
+
+//---let---
+
+// let does not allow redeclaration of same variable
+//let doesnot support hoisting 
+// let is block scope always not global at all
 
 
+// let a  = "Ahmed";
 
+// let a = "zain";
+
+//not allowing redeclaration
+
+
+// console.log(b)
+
+// let b = "hello"
+
+//not allowing hoisting
+
+var initial = "example of scope"
+function scope(){
+    if(initial){
+        let ex = initial 
+        var example = initial
+    }
+    console.log(example)
+    console.log(ex)
+}
+
+     
+scope()
+
+
+//let is proving to be block scope

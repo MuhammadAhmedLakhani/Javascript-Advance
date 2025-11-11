2511097f69762b7c06b05947af05b7107effd26a
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-11 20:21:05 +0500

    Eventloop:half concept V1

---
+++ eventLoop.js
@@ -0,0 +1,21 @@
+
+//setTimeout function is a browser timer function
+//it is the exampple of asyncronous programming as
+//code after setTimeout continues to runs and doesnot wait for it
+// to run 
+//by default js is syncronous  programming language
+
+
+console.log("Hello")
+
+setTimeout(function(){
+    console.log("waiting")
+},1000)
+
+
+
+console.log("Hye")
+
+
+
+

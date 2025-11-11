7039597ab668e51d2e1eeb3e02564bc338645bee
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-11 19:58:16 +0500

    Variables:var issues V-2

--- variable.js
+++ variable.js
@@ -4,14 +4,23 @@
 //it is a isuue as when using same variable throughout confusion create confusion
 //like expecting Ahmed but getting Muhammad in lin 14
 
-var a = "Ahmed";
+// var a = "Ahmed";
+
+// console.log(a)
+
+
+// var a = "Muhammad"
+
+// console.log("Welcome  " + a)
+
+
+// --v2
+
+// var support housting which means variable declared with var come on top of program 
 
 console.log(a)
 
-
-var a = "Muhammad"
-
-console.log("Welcome  " + a)
+var a = "Ahmed"
 
 
 

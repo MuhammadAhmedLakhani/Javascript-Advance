2e3419b2d09dce50aa27bebc6b3dc1f205d85626
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-12 19:05:11 +0500

    Objects:not enhanced

---
+++ enhanceObject.js
@@ -0,0 +1,21 @@
+
+
+//--enhanced objects
+
+//if you want to make object from variables
+//you are repeating the code as the key and value are same
+//like in example below
+
+
+
+
+let name = "Ahmed";
+
+let school = "SMIT";
+
+let obj = {
+    name:name,
+    school:school
+}
+
+console.log(obj)

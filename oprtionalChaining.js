85015ecb8dceef520d5156fc39d989ff68c7701a
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-12 19:31:35 +0500

    OptionalChaining:Applied:

--- oprtionalChaining.js
+++ oprtionalChaining.js
@@ -6,6 +6,10 @@
 //this is an issue that ther is an issue in object properties(like calling those properties which are not present) then whole program 
 //crashes and remaining code donot run at all
 
+//now in optionalchaining you put "?" before the object property which you are unsured that it is present or not
+//this question mark (?) prevent the javascript the produce error if there is an error and undefined id return
+//this is great help as it stops the program from crashing and remaining code do runs as it should be
+
 
 let obj = {
     student:{
@@ -15,6 +19,6 @@ let obj = {
 }
 
 
-console.log(obj.student.school.details.name)
+console.log(obj?.student?.school?.details?.name)
 
 console.log(obj.student.name)

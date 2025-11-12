7dcec0c5cdbecd16a60b886d9275295c5e23a1a7
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-12 17:45:45 +0500

    Copy:shallow copy

--- copy.js
+++ copy.js
@@ -7,6 +7,13 @@
 
 //if change one object the same changes reflect back in all 3 objects which is an issue
 
+//but there is a way to make the new object as copy of original 
+//without shsring its reference in memory
+//this is really helpful as if will prevent the changes tobe reflect in all three objects when we change one
+//as in this example obj2 has new reference through help of spread operator(issue gets solved partially)
+//this copying method is known as "shallow copy"
+
+
 
 
 
@@ -20,7 +27,7 @@ let obj = {
 
 let obj1 = obj
 
-let obj2 = obj1
+let obj2 = {...obj1}
 
 obj2.name = "Ahad"
 console.log(obj,obj1,obj2)

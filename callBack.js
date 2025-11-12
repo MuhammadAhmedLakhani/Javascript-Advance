7395de997c892becc1d84a850e59d5f3b8fe3abe
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-12 20:35:08 +0500

    Callback function:V1

---
+++ callBack.js
@@ -0,0 +1,27 @@
+
+
+
+//---call back function
+
+//we in real life call someone and if the other person is busy and doesnot respond at that time
+// when the other person call back when free
+//similar concept happens in js 
+//when we call a function and it is busy it doenot respond
+//but when free and inorder to inform/do something when free it need another function to tell its free and finishes its tasks of whatever it was busy on 
+//this function to inform the free status is called "callback" function
+//call back function is itself a function and passsed to function
+//here in this example function  named "callBack()" is itself a callback function ans is passed as an argument to abc function
+
+
+function abc(cb){
+    setTimeout(function(){
+        console.log("Done")
+        cb()
+    },5000)
+
+}
+function callBack(){
+    console.log("abc free hogaya hai")
+}
+
+abc(callBack)

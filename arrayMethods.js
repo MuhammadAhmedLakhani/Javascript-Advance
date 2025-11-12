c25718eb194a3db49db986a6836c415b0ee38277
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-12 21:24:02 +0500

    ArrayMethods:map() method

--- arrayMethods.js
+++ arrayMethods.js
@@ -8,10 +8,26 @@
 //it does not return anything
 //call back function first argument is array element while second argument is index and thirs optional argument is array it self
 
+// var arr = ["Karachi","Lahore","Islamabad"];
+
+
+
+// arr.forEach(function(elem,i){
+//     console.log(elem,i)
+// })
+
+
+
+// ---map-method()
+//same as forEach but with iteration it also return new array
+//it transform the entire array
+
+
 var arr = ["Karachi","Lahore","Islamabad"];
 
-
-
-arr.forEach(function(elem,i){
-    console.log(elem,i)
+let result = arr.map(function(elem){
+    return `${elem} , Pakistan`
 })
+
+
+console.log(result)

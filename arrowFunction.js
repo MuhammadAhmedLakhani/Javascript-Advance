5a3919def79b2f1e4a532fb01abb580576562dfe
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-14 14:30:22 +0500

    Arrowfunctions:Normal function Hoisting

---
+++ arrowFunction.js
@@ -0,0 +1,16 @@
+
+//--arrow function
+
+
+//like let variable normal function also has housting problem 
+//in example below function is declared below and called before declaration still it is acesible 
+//housting can cause confusion in flow of program
+
+
+greet()
+
+function greet(){
+    console.log("Hello")
+}
+
+

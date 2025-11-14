91dc72f705d1c590e3261b8c147ab0a5cfb8ec86
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-14 22:19:33 +0500

    Modules:Import/Export V1

---
+++ modules.js
@@ -0,0 +1,23 @@
+
+//--modules
+
+//if you want some function to be available in another file then modules can be used 
+
+//export the desired function as an object(curley brakets)
+
+//recieves the function in another file through import {} in object style followed by from destination file
+
+//donot forget to include typ;e module in script tag as well
+
+
+
+
+
+let getDate = ()=>{
+   return  new Date().toDateString()
+}
+
+
+export {
+    getDate
+}
---
+++ modules2.js
@@ -0,0 +1,5 @@
+import { getDate } from "./modules.js";
+
+
+
+console.log(getDate())

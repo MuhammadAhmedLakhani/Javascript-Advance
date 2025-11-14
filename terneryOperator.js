c44aa7ab4f3e8ef4b9f96fb45f9fc72e762a1c63
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-14 20:46:00 +0500

    Ternery Operator

---
+++ terneryOperator.js
@@ -0,0 +1,13 @@
+
+//--TerneryOperator
+
+//it is short way of setting conditions
+//condition is setup if condition is true then block after question mark runs and if condition is false then else block runs after colon mark
+
+
+
+let age = 20;
+
+let result = age >= 20 ? "this condition is  true":"else condition";
+
+console.log(result)

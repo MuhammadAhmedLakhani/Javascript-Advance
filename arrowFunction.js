d943bacf09bd8ab5e8101a4131fcfbb0e3e4935a
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-14 14:47:55 +0500

    Arrowfunction:Syntax

--- arrowFunction.js
+++ arrowFunction.js
@@ -2,15 +2,24 @@
 //--arrow function
 
 
-//like let variable normal function also has housting problem 
+//like let variable normal function also has hoisting problem as normal functions are fully hoisted (name and body as well)
 //in example below function is declared below and called before declaration still it is acesible 
-//housting can cause confusion in flow of program
+//hoisting can cause confusion in flow of program
+
+//hoisting problem can be solved if we put the function inside a variable 
+
+// if you remove the function keyword and replace it with arrow(=>) then its an arrow function
+
+// arrow function can only  be declared as expression (inside variable)
 
 
-greet()
 
-function greet(){
-    console.log("Hello")
+let f = (n) => {
+    console.log(n)
 }
+
+f("ahmed")
+
+
 
 

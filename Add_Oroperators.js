1d5acb988245d09e52e82316f62c313860d8304b
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-14 21:09:40 +0500

    And/Or:assignment V2

--- Add_Oroperators.js
+++ Add_Oroperators.js
@@ -6,8 +6,22 @@
 //if age value is 0 then it is false so or(||) operator will check next value ("not available") in quest of true 
 //as a result result will have a "not available as value"
 
-let  age = 0;
+// let  age = 0;
 
-let result = age || "not available"
+// let result = age || "not available"
+
+// console.log(result)
+
+//--And operator(&&)
+
+//as we know that and operator looks for both true values and if it encounters one false value it donot go aheasd and return 
+//in below example the final output will be "age is available"
+//it is because  age is true so && operator go ahea din search of next true value and as a result "age is available" is assigned 
+
+
+
+let age = 20
+
+let result = age && "age is  available"
 
 console.log(result)

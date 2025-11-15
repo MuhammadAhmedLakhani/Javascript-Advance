d9e1797a42d22c5872f3bcde5f7fe3665737355b
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-15 14:25:16 +0500

    Modules:steric(*) feature

--- modules2.js
+++ modules2.js
@@ -1,29 +1,44 @@
-import {getDate as Date ,  getTime , appName } from "./modules.js";
+// import {getDate as Date ,  getTime , appName } from "./modules.js";
 
-import date from "./modules.js"
+// import date from "./modules.js"
+
+// -- steric all
+
+// using steric(*) in import and saving it as a variable like all moves the all exported items to this new variable in our case its "all" 
+
+//now while acess it you need to behave like all is object which it is and acess the itsms using . notation like below
+
+
+import * as all from "./modules.js"
+
+console.log(all.default())
+
+
+
+
+
+
+
+
+
+// let getDate  = () => {
+//     return "getDate duplicate"
+
+// }
+
+
+
+// console.log(getDate())
+
+// console.log(Date(),"alisas one")
+
+
+
 
 //there is a possiblity that the imported method and new method you created in modules2.js file have same name 
 //in order distinguish them you can use "as" keyword and give it a new alias name
 
 //in this way both methods are acessible as depicted below
-
-
-
-
-let getDate  = () => {
-    return "getDate duplicate"
-
-}
-
-
-
-console.log(getDate())
-
-console.log(Date(),"alisas one")
-
-
-
-
 
 
 

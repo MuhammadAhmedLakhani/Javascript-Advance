d59a4792710719e38762a07a828c63c0258cf2db
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-15 15:05:35 +0500

    Api:Theory

---
+++ api.js
@@ -0,0 +1,18 @@
+
+//Frontend  need data for interactive webapps
+//data can be accessed directlr form front end to backend efficiently
+
+//instead backend app is made as it can only talk witha server as server doesnot communicate with front end due to security issues
+//but anway front end need date to display or send data to server 
+//for solving this issue API aarives with a bang
+// (Application Porgramming Interface)
+//api is the bridge btw front and back end
+
+//there is a protocol for api communication from browser called "Https" (HyperText Transfer Protocol)
+
+//HTTPS has methods like Get  Post Put etc
+
+// .Get is used for getting data from server
+// .Posr is used for sending data to server
+// .put and .fetch is used for updating data at server
+//Api has alot of methods
